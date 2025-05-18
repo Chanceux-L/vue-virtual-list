56f394a52c291773642b09e44da391fbdbfa2c80
Chanceux-L <91234122+Chanceux-L@users.noreply.github.com>
2025-05-18 09:16:05 +0000

    Deploying to gh-pages from @ Chanceux-L/vue-virtual-list@7f22fb8f913e1970dee39a3fbfa2d5ba1b92a65a 🚀

---
+++ demo/index.vue.d.ts
@@ -0,0 +1,9 @@
+import { VirtualListInstance } from '../src/components/types.ts';
+declare const _default: import('vue').DefineComponent<{}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, true, {
+    listRef: import('vue').ShallowUnwrapRef<VirtualListInstance<{
+        id: number;
+        title: string;
+        content: string;
+    }>> | null;
+}, HTMLDivElement>;
+export default _default;
